effc5976ab9f69095ce8e702e9e6349181833396
Manan-0107 <mananshah1127@gmail.com>
2025-08-13 18:23:29 +0530

    Update 24.c

--- 24.c
+++ 24.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() {
     int a,b,c;
-    printf("enter the marks of first subject");
+    printf("enter the first value");
     scanf("%d",&a);
-    printf("enter the marks of second subject");
+    printf("enter the second value");
     scanf("%d",&b);
     c=a+b;
     a=c-a;
     b=c-b;
     printf("after swaping value we get are %d and %d",a,b);
     return 0;
+
 }
