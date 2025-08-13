14abab872282e92a2fdd1aa5a32f2a962891554c
Manan-0107 <mananshah1127@gmail.com>
2025-08-13 18:16:33 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -5,7 +5,8 @@ int main() {
     printf("enter the fahrenheit which you want to convert into celsius");
     scanf("%f",&a);
     b=5*(a/9) - 32;
-    printf("the amount in fahrenheit %f\n",b);
+    printf("the amount in celsius %f\n",b);
     
     return 0;
+
 }
